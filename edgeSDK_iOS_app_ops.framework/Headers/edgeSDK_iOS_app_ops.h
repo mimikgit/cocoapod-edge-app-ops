2449ecb75d04881606c9dfeb7d9099f86c15204c
Raduz Benicky <raduz.benicky@mimik.com>
2018-07-24 12:37:05 -0700

    0.0.1 initial commit

---
+++ edgeSDK_iOS_app_ops.framework/Headers/edgeSDK_iOS_app_ops.h
@@ -0,0 +1,19 @@
+//
+//  edgeSDK_iOS_app_ops.h
+//  edgeSDK-iOS-app-ops
+//
+//  Created by Radúz Benický on 2018-07-18.
+//  Copyright © 2018 mimik. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for edgeSDK_iOS_app_ops.
+FOUNDATION_EXPORT double edgeSDK_iOS_app_opsVersionNumber;
+
+//! Project version string for edgeSDK_iOS_app_ops.
+FOUNDATION_EXPORT const unsigned char edgeSDK_iOS_app_opsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <edgeSDK_iOS_app_ops/PublicHeader.h>
+
+
